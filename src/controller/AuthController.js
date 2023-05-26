fa1861d5570c263712d40c2b9111129596a88a09
Vitor Falcao <joaofalcao.33@outlook.com>
2023-05-26 11:05:11 -0300

    install bcryptjs para hash da senha

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,15 +1,28 @@
 // faz a operação de comparação da model
 const express = require('express');
 
-const UserModel = require('../models/User') /* importa model usuario */
+const UserModel = require('../models/User'); /* importa model usuario */
 
 const router = express.Router();   /* Para criar a rota */
 
-router.post("/register", (req, res)=>{  /* Quando vier um post da rota register */
-    console.log(req.body);
+router.post("/register", async (req, res) => {  /* Quando vier um post da rota register */
+
+    const { email } = req.body
+    if (await UserModel.findOne({ email })) {
+        return res.status(400).json({   // envia resposta com status 4400 e o json  
+            error: true,
+            message: "User already exists"
+        })
+    }
+
+    const User = await UserModel.create(req.body)  // Cria no mongo o registro do modelo usuário com os dados de req.body
+
+    User.password = undefined; // Remove password do User quando create
+
     return res.json({
         error: false,
         message: "Registered with success",
+        data: User
     })
 })
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('../database/index')  // conexão já foi feita na database/index. mongoose exportado.
 
+const bcryptjs = require("bcryptjs")
+
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -20,6 +22,10 @@ const UserSchema = new mongoose.Schema({
         type: Date,     // campo do tipo data
         default: Date.now // data padrao da hora em que foi cadastrado
     }
+})
+UserSchema.pre("save", async function (next) { // antes do save executa o hash
+    const hash = await bcryptjs.hash( this.password, 10) // função do bcryptjs que cria o hash 10*
+    this.password = hash // atribui ao password o hash
 })
 
 const User = mongoose.model("User", UserSchema); // nome da model e qual seu esquema
